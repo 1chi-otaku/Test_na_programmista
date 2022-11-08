3748488c6d3748bfabee3f2eeb9bb27c854cf189
Serhii <mywaifuqueen@gmail.com>
2022-11-08 14:05:46 +0000

    Add project files.

---
+++ RadioButton/RadioButton.cpp
@@ -0,0 +1,105 @@
+﻿#include <windows.h>
+#include <tchar.h>
+#include "resource.h"
+
+BOOL CALLBACK DlgProc(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPTSTR lpszCmdLine, int nCmdShow)
+{
+	return DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG2), NULL, (DLGPROC)DlgProc);
+}
+
+HWND radio[30];
+
+HWND hAnswer1, hAnswer2, hAnswer3;
+TCHAR Answer1[25], Answer2[25], Answer3[25];
+
+BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wp, LPARAM lp)
+{
+	TCHAR str[100];
+	const double questions = 13;
+	static int right_answers = 0;
+	switch (message)
+	{
+	case WM_CLOSE:
+		EndDialog(hWnd, 0);
+		return TRUE;
+
+	case  WM_INITDIALOG:
+		for (int i = 0; i < 30; i++)
+		{
+			radio[i] = GetDlgItem(hWnd, IDC_RADIO1+i);
+		}
+
+		hAnswer1 = GetDlgItem(hWnd, IDC_EDIT3);
+		hAnswer2 = GetDlgItem(hWnd, IDC_EDIT5);
+		hAnswer3 = GetDlgItem(hWnd, IDC_EDIT4);
+		return TRUE;
+
+	case WM_COMMAND:
+
+		if (LOWORD(wp) == IDOK)
+		{
+			LRESULT lResult = SendMessage(radio[0], BM_GETCHECK, 0, 0);
+			LRESULT RResult;
+			LRESULT MResult;
+
+			if (lResult == BST_CHECKED) right_answers++;
+			lResult = SendMessage(radio[3], BM_GETCHECK, 0, 0);
+			if (lResult == BST_CHECKED) right_answers++;
+			lResult = SendMessage(radio[7], BM_GETCHECK, 0, 0);
+			if (lResult == BST_CHECKED) right_answers++;
+			lResult = SendMessage(radio[9], BM_GETCHECK, 0, 0);
+			if (lResult == BST_CHECKED) right_answers++;
+			lResult = SendMessage(radio[13], BM_GETCHECK, 0, 0);
+			if (lResult == BST_CHECKED) right_answers++;
+			lResult = SendMessage(radio[17], BM_GETCHECK, 0, 0);
+			if (lResult == BST_CHECKED) right_answers++;
+			lResult = SendMessage(radio[20], BM_GETCHECK, 0, 0);
+			if (lResult == BST_CHECKED) right_answers++;
+
+			lResult = SendMessage(radio[21], BM_GETCHECK, 0, 0);
+			MResult = SendMessage(radio[22], BM_GETCHECK, 0, 0);
+			RResult = SendMessage(radio[23], BM_GETCHECK, 0, 0);
+			if (lResult == BST_CHECKED && RResult == BST_CHECKED && MResult != BST_CHECKED) right_answers++;
+
+			lResult = SendMessage(radio[24], BM_GETCHECK, 0, 0);
+			MResult = SendMessage(radio[25], BM_GETCHECK, 0, 0);
+			RResult = SendMessage(radio[26], BM_GETCHECK, 0, 0);
+			if (lResult == BST_CHECKED && RResult == BST_CHECKED && MResult != BST_CHECKED) right_answers++;
+
+
+			lResult = SendMessage(radio[27], BM_GETCHECK, 0, 0);
+			MResult = SendMessage(radio[28], BM_GETCHECK, 0, 0);
+			RResult = SendMessage(radio[29], BM_GETCHECK, 0, 0);
+			if (lResult == BST_CHECKED && MResult == BST_CHECKED && RResult != BST_CHECKED) right_answers++;
+
+			GetWindowText(hAnswer1, Answer1, 25);
+			GetWindowText(hAnswer2, Answer2, 25);
+			GetWindowText(hAnswer3, Answer3, 25);
+		
+			if (!lstrcmpi(Answer1, _T("Hello!"))) {
+				right_answers++;
+			}
+
+			if (!lstrcmpi(Answer2, _T("goto"))) {
+				right_answers++;
+			}
+
+			if (!lstrcmpi(Answer3, _T("switch"))) {
+				right_answers++;
+			}
+
+			
+			double result = (right_answers / questions) * 100;
+
+			_stprintf_s(str, TEXT("Вы набрали - %.2f/100"), result);
+			MessageBox(hWnd, str, TEXT("Результаты"), MB_OK | MB_ICONINFORMATION);
+			right_answers = 0;
+
+			
+		}
+		return TRUE;
+	}
+	return FALSE;
+}
---
+++ RadioButton/resource.h
@@ -0,0 +1,60 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by RadioButton.rc
+//
+#define IDD_DIALOG1                     101
+#define IDD_DIALOG2                     103
+#define IDB_BITMAP1                     105
+#define IDB_BITMAP2                     106
+#define IDB_BITMAP3                     107
+#define IDC_BUTTON1                     1001
+#define IDC_EDIT1                       1002
+#define IDC_PROGRESS1                   1004
+#define IDC_PROGRESS2                   1005
+#define IDC_COMBO1                      1006
+#define IDC_LIST1                       1007
+#define IDC_RADIO1                      1008
+#define IDC_RADIO2                      1009
+#define IDC_RADIO3                      1010
+#define IDC_RADIO4                      1011
+#define IDC_RADIO5                      1012
+#define IDC_RADIO6                      1013
+#define IDC_RADIO7                      1014
+#define IDC_RADIO8                      1015
+#define IDC_RADIO9                      1016
+#define IDC_RADIO10                     1017
+#define IDC_RADIO11                     1018
+#define IDC_RADIO12                     1019
+#define IDC_RADIO13                     1020
+#define IDC_RADIO14                     1021
+#define IDC_RADIO15                     1022
+#define IDC_RADIO16                     1023
+#define IDC_RADIO17                     1024
+#define IDC_RADIO18                     1025
+#define IDC_RADIO19                     1026
+#define IDC_RADIO20                     1027
+#define IDC_RADIO21                     1028
+
+#define IDC_CHECK22                     1029
+#define IDC_CHECK23                     1030
+#define IDC_CHECK24                     1031
+#define IDC_CHECK25                     1032
+#define IDC_CHECK26                     1033
+#define IDC_CHECK27                     1034
+#define IDC_CHECK28                     1035
+#define IDC_CHECK29                     1036
+#define IDC_CHECK30                     1037
+#define IDC_EDIT3                       10293
+#define IDC_EDIT4                       10300
+#define IDC_EDIT5                       10342
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        108
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         10309
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
